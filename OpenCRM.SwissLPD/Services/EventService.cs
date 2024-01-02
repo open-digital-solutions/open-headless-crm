4bed41611232d6228e4ef25c2b41389f64caf1a8
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2024-01-02 17:21:07 +0100

    Add SwissLDP Event Service definition

---
+++ OpenCRM.SwissLPD/Services/EventService.cs
@@ -0,0 +1,43 @@
+﻿using OpenCRM.Core.DataBlock;
+using OpenDHS.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCRM.SwissLPD.Services
+{
+        public class EventService<TDBContext> : IEventService where TDBContext : DataContext
+        {
+        public readonly IDataBlockService _dataBlockService;
+        public EventService(IDataBlockService dataBlockService)
+        {
+            _dataBlockService = dataBlockService;
+        }
+        public DataBlockModel<EventModel> AddEvent(DataBlockModel<EventModel> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataBlockModel<EventModel> GetEvent(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DataBlockModel<EventModel>> GetEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveEvent(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Seed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
