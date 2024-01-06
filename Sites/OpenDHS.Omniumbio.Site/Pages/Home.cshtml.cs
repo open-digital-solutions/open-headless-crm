6551576f6f1915eb4de4e7450f271ee9e5b23153
Yariel Rodriguez <41107774+yarielre@users.noreply.github.com>
2024-01-06 11:08:27 +0100

    Update OmnBio pages path & styles

--- Sites/OpenDHS.Omniumbio.Site/Pages/Home.cshtml.cs
+++ Sites/OpenDHS.Omniumbio.Site/Pages/Home.cshtml.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace OpenDHS.Omniumbio.Site.Pages
+namespace WGOOmniumbio.Pages
 {
-    public class HomeModel : PageModel
+    public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
