b9a47a79b53604d400353716ae5c896bad750e73
Grettel Hernández Garbey <91501281+grettelHdez@users.noreply.github.com>
2024-01-09 10:06:44 -0500

    Added TablePartial

--- OpenCRM.SwissLPD/Areas/SwissLDP/Pages/Event/Index.cshtml.cs
+++ OpenCRM.SwissLPD/Areas/SwissLDP/Pages/Event/Index.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using OpenCRM.Core.DataBlock;
 using OpenCRM.Core.Web.Models;
 using OpenCRM.Core.Web.Pages;
+using OpenCRM.Core.Web.Pages.Shared;
 using OpenCRM.SwissLPD.Services;
 
 namespace OpenCRM.SwissLPD.Areas.SwissLDP.Pages.Event
@@ -19,9 +20,17 @@ namespace OpenCRM.SwissLPD.Areas.SwissLDP.Pages.Event
         [BindProperty]
         public _BreadCrumbPartialModel BreadCrumbPartialModel { get; set; } = new _BreadCrumbPartialModel();
 
+        [BindProperty]
+        public _TablePartialModel TablePartialModel { get; set; } = new _TablePartialModel();
+
         public IndexModel(IEventService eventService)
         {
             _eventService = eventService;
+
+            TablePartialModel.Headers.Add("ID");
+            TablePartialModel.Headers.Add("Description");
+            TablePartialModel.Headers.Add("StartDate");
+            TablePartialModel.Headers.Add("EndDate");
         }
 
         public void OnGet()
@@ -29,18 +38,18 @@ namespace OpenCRM.SwissLPD.Areas.SwissLDP.Pages.Event
             var events = _eventService.GetEvents();
             if (events != null)
             {
-                EventList = events;
+                //EventList = events;
+                foreach(var item in events)
+                {
+                    TablePartialModel.Lines.Add(new List<string>()
+                    {
+                        item.ID.ToString(),
+                        item.Data.Description,
+                        item.Data.StartDate.ToString(),
+                        item.Data.EndDate.ToString()
+                    });
+                }
             }
-
-            BreadCrumbLinkModel newLink = new BreadCrumbLinkModel()
-            {
-                Area = "",
-                IsActive = true,
-                Name = "Event",
-                Page = "/Event"
-            };
-
-            BreadCrumbPartialModel.Links.Add(newLink);
         }
     }
 }
